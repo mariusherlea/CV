9f4c447e3a769b57b4b5578d4051f26c45aeb0c8
mariusherlea <mariusherlea@gmail.com>
2023-08-08 08:40:53 +0300

    add dinamic button on menu

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
 
-let menu={
-    button:3,
-
-    //special key names
+let content={
     'background-color':'yellow',
-
-
     render:function(){
         let p_content=document.getElementById('eu').innerHTML;
-        console.log(p_content);
-        console.log(menu);
+        const cevaCS=document.getElementById('menu');
+
+        // console.log(p_content);
         
-        console.log(menu['background-color']);
+        // console.log(content['background-color']);
 
         if(p_content=='Home'){
-            document.getElementById('eu').innerHTML='Willkommen';
+            document.getElementById('eu').innerHTML='Marius Herlea HomePage. Willkommen !';
 
-            //backgroundColor = background-color
-        const cevaCS=document.getElementById('cevaCSS');
-        cevaCS.style.backgroundColor=this["background-color"];
-        document.getElementById('eu').style.backgroundColor='blue';
+            // backgroundColor = background-color
+            
+            cevaCS.style.backgroundColor=this["background-color"];
+            document.getElementById('eu').style.backgroundColor='blue';
 
         } else if(p_content=='CV'){
-            document.getElementById('eu').innerHTML='CVC';
+
+            document.getElementById('eu').innerHTML='Während ich arbeite, ist der Lebenslauf veraltet :)';
+
+            cevaCS.style.backgroundColor="cyan";
+            document.getElementById('eu').style.backgroundColor='green';
+        
         } else {
 
         }
     
-    }
+    },
+
 }
+
+
+let menuPrint={
+    button:3,
+
+    //special key names
+
+    print: function(){
+        var ul=document.getElementById('menu');
+        var li=document.createElement('button');
+        li.appendChild(document.createTextNode('Home'))
+        ul.appendChild(li);
+    }
+    
+}
+
+window.addEventListener('load',myInit, true);
+
+function myInit(){
+    content.render();
+    menuPrint.print();
+}
+
+// myInit();
