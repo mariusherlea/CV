23b0055364198775fb2f5bda01541aa50122723d
mariusherlea <mariusherlea@gmail.com>
2023-08-10 13:44:08 +0300

    add list of object and render it experiences

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let header={
 
         } else if(p_content=='CV'){
 
-            document.getElementById('header').innerHTML='Während ich arbeite, ist der Lebenslauf veraltet :)';
+            document.getElementById('header').innerHTML='Curricullum Vitae';
 
             cevaCS.style.backgroundColor="cyan";
             document.getElementById('header').style.backgroundColor='green';
@@ -60,20 +60,90 @@ let content={
 
 let ageCalculation={
 
-    calcAge:function(birthYear){
-        document.getElementById('age').innerHTML = new Date().getFullYear() - birthYear ;
-    },
     addCurrentYear:function(){
         document.getElementById('curentYear').innerHTML = new Date().getFullYear();
     }
     
 }
 
+const experienceList={
+    experience:[
+        {
+            period:'Apr 2016 - Nov 2019',
+            position:'Forklift operator',
+            company:'KHUENE+NAGEL',
+            workTown:'Sebes',
+            fieldOfActivities:'Logistics'
+        },
+
+        {
+            period:'Mar 2014 - Feb 2015',
+            position:'Deputy Chief Shop',
+            company:'Billa',
+            workTown:'Sebes',
+            fieldOfActivities:'Sales'
+        },
+
+        {
+            period:'Feb 2013 - Aug 2013',
+            position:'Chief Shop',
+            company:'Profi',
+            workTown:'Abrud',
+            fieldOfActivities:'Sales'
+        },
+
+        {
+            period:'Aug 2012 - Aug 2013',
+            position:'Deputy Chief Shop',
+            company:'Penny Market',
+            workTown:'Sebes',
+            fieldOfActivities:'Sales'
+        },
+
+        {
+            period:'Feb 2010 - Aug 2011',
+            position:'Commercial worker',
+            company:'Kaufland',
+            workTown:'Orastie',
+            fieldOfActivities:'Sales'
+        },
+
+        {
+            period:'Mar 2000 - Oct 2008',
+            position:'Copy Editor',
+            company:'Alba Iulia University',
+            workTown:'Alba Iulia',
+            fieldOfActivities:'Printing / Publishing'
+        }
+
+    ],
+
+    render(){
+        const renderHook=document.getElementById('experience');
+        const expList=document.createElement('ol');
+        for(exp of this.experience){
+            const expEl=document.createElement('li');
+            expEl.innerHTML=`
+            <div>
+                <h2>${exp.period}</h2>
+                <h3>${exp.position}</h3>
+                <p>Company: ${exp.company}</p>
+                <p>WorkTown: ${exp.workTown}</p>
+                <p>Fields of activities: ${exp.fieldOfActivities}</p>
+            </div>
+            `;
+            expList.append(expEl);
+        }
+        renderHook.append(expList);
+    }
+};
+
+
 window.addEventListener('load',myInit, true);
 
 function myInit(){
     header.render();
     content.print();
-    ageCalculation.calcAge(1977);
     ageCalculation.addCurrentYear();
+    experienceList.render();
 }
