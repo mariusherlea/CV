f5fc410ae25dd17888ed03af59888b1e00b4ee54
mariusherlea <mariusherlea@gmail.com>
2023-08-15 09:52:11 +0300

    a more readable date_start date_end

--- cv.js
+++ cv.js
@@ -8,8 +8,8 @@ class Experience {
     fieldOfActivities;
 
     constructor(date_start, date_end, position, company, workTown, fieldOfActivities){
-        this.date_start=date_start;
-        this.date_end=date_end;
+        this.date_start=new Date(date_start).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+        this.date_end=new Date(date_end).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
         this.position=position;
         this.company=company;
         this.workTown=workTown;
@@ -17,19 +17,21 @@ class Experience {
     }
 }
 
+
+
 const experienceList={
     experience:[
         forklift=new Experience(`${new Date('2020-01-10T14:00:00')}`,`${new Date('2023-11-10T14:00:00')}`,'Forklift operator','KHUENE+NAGEL','Sebes','Logistics'),
 
-        // deputyBilla=new Experience('Mar 2014 - Feb 2015','Deputy Chief Shop','Billa','Sebes','Sales'),
+        deputyBilla=new Experience(`${new Date('2018-01-10T14:00:00')}`,`${new Date('2019-11-10T14:00:00')}`,'Deputy Chief Shop','Billa','Sebes','Sales'),
 
-        // chiefProfi=new Experience('Feb 2013 - Aug 2013', 'Chief Shop', 'Abrud', 'Sales'),
+        chiefProfi=new Experience(`${new Date('2016-01-10T14:00:00')}`,`${new Date('2018-11-10T14:00:00')}`, 'Chief Shop', 'Profi','Abrud', 'Sales'),
 
-        // deputyPenny=new Experience('Aug 2012 - Aug 2013','Deputy Chief Shop','Penny Market','Sebes','Sales'),
+        deputyPenny=new Experience(`${new Date('2013-01-10T14:00:00')}`,`${new Date('2014-11-10T14:00:00')}`,'Deputy Chief Shop','Penny Market','Sebes','Sales'),
 
-        // workerKaufland=new Experience('Feb 2010 - Aug 2011','Commercial worker','Kaufland','Orastie','Sales'),
+        workerKaufland=new Experience(`${new Date('2009-08-10T14:00:00')}`,`${new Date('2010-06-10T14:00:00')}`,'Commercial worker','Kaufland','Orastie','Sales'),
 
-        // editorUniv=new Experience('Mar 2000 - Oct 2008','Copy Editor','Alba Iulia University','Alba Iulia','Printing / Publishing')
+        editorUniv=new Experience(`${new Date('2000-03-10T14:00:00')}`,`${new Date('2008-11-10T14:00:00')}`,'Copy Editor','Alba Iulia University','Alba Iulia','Printing / Publishing')
 
     ],
 
@@ -43,6 +45,7 @@ const experienceList={
         const millisecondsInMinute = 60 * millisecondsInSecond;
         const millisecondsInHour = 60 * millisecondsInMinute;
         const millisecondsInDay = 24 * millisecondsInHour;
+        
 
         const days = Math.floor(timeDifference / millisecondsInDay);
         const hours = Math.floor((timeDifference % millisecondsInDay) / millisecondsInHour);
@@ -66,9 +69,8 @@ const experienceList={
                 <p>Company: ${exp.company}</p>
                 <p>WorkTown: ${exp.workTown}</p>
                 <p>Fields of activities: ${exp.fieldOfActivities}</p>
-                ${console.log(this.calculateTime(new Date(exp.date_start),new Date(exp.date_end)))}
-            </div>
-            
+                <p>${(this.calculateTime(new Date(exp.date_start),new Date(exp.date_end)))}</p>
+            </div>            
             `;
             expList.append(expEl);
         }
