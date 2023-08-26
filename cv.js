b3c79e831268401aaf8c92f7ef1c239c189ae9ff
mariusherlea <mariusherlea@gmail.com>
2023-08-26 11:48:56 +0300

    age calc added

--- cv.js
+++ cv.js
@@ -1,94 +1,135 @@
 class Experience {
+  date_start;
+  date_end;
+  position;
+  company;
+  workTown;
+  fieldOfActivities;
 
-    date_start;
-    date_end;
-    position;
-    company;
-    workTown;
-    fieldOfActivities;
-
-    constructor(date_start, date_end, position, company, workTown, fieldOfActivities){
-        this.date_start=new Date(date_start).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
-        this.date_end=new Date(date_end).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
-        this.position=position;
-        this.company=company;
-        this.workTown=workTown;
-        this.fieldOfActivities=fieldOfActivities;
-    }
-
-    
+  constructor(
+    date_start,
+    date_end,
+    position,
+    company,
+    workTown,
+    fieldOfActivities
+  ) {
+    this.date_start = new Date(date_start).toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+    this.date_end = new Date(date_end).toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+    this.position = position;
+    this.company = company;
+    this.workTown = workTown;
+    this.fieldOfActivities = fieldOfActivities;
+  }
 }
 
+const experienceList = {
+  experience: [
+    (forklift = new Experience(
+      `${new Date("2020-01-10T14:00:00")}`,
+      `${new Date("2023-11-10T14:00:00")}`,
+      "Forklift operator",
+      "KHUENE+NAGEL",
+      "Sebes",
+      "Logistics"
+    )),
+    (deputyBilla = new Experience(
+      `${new Date("2018-01-10T14:00:00")}`,
+      `${new Date("2019-11-10T14:00:00")}`,
+      "Deputy Chief Shop",
+      "Billa",
+      "Sebes",
+      "Sales"
+    )),
+    (chiefProfi = new Experience(
+      `${new Date("2016-01-10T14:00:00")}`,
+      `${new Date("2018-11-10T14:00:00")}`,
+      "Chief Shop",
+      "Profi",
+      "Abrud",
+      "Sales"
+    )),
+    (deputyPenny = new Experience(
+      `${new Date("2013-01-10T14:00:00")}`,
+      `${new Date("2014-11-10T14:00:00")}`,
+      "Deputy Chief Shop",
+      "Penny Market",
+      "Sebes",
+      "Sales"
+    )),
+    (workerKaufland = new Experience(
+      `${new Date("2009-08-10T14:00:00")}`,
+      `${new Date("2010-06-10T14:00:00")}`,
+      "Commercial worker",
+      "Kaufland",
+      "Orastie",
+      "Sales"
+    )),
+    (editorUniv = new Experience(
+      `${new Date("2000-03-10T14:00:00")}`,
+      `${new Date("2008-11-10T14:00:00")}`,
+      "Copy Editor",
+      "Alba Iulia University",
+      "Alba Iulia",
+      "Printing / Publishing"
+    )),
+  ],
 
+  calculateTime(date_start, date_end) {
+    let distance = Math.abs(date_end - date_start);
 
-const experienceList={
-    experience:[
-        forklift=new Experience(`${new Date('2020-01-10T14:00:00')}`,`${new Date('2023-11-10T14:00:00')}`,'Forklift operator','KHUENE+NAGEL','Sebes','Logistics'),
+    const years = Math.floor(distance / (3600000 * 24 * 30 * 12));
+    distance -= years * (3600000 * 24 * 30 * 12);
+    const months = Math.floor(distance / (3600000 * 24 * 30));
+    distance -= months * (3600000 * 24 * 30);
+    const days = Math.floor(distance / (3600000 * 24));
+    distance -= days * (3600000 * 24);
+    const hours = Math.floor(distance / 3600000);
+    distance -= hours * 3600000;
+    const minutes = Math.floor(distance / 60000);
+    distance -= minutes * 60000;
+    const seconds = Math.floor(distance / 1000);
+    return ` ${years} years ${months} month ${days} days `;
+  },
 
-        deputyBilla=new Experience(`${new Date('2018-01-10T14:00:00')}`,`${new Date('2019-11-10T14:00:00')}`,'Deputy Chief Shop','Billa','Sebes','Sales'),
-
-        chiefProfi=new Experience(`${new Date('2016-01-10T14:00:00')}`,`${new Date('2018-11-10T14:00:00')}`, 'Chief Shop', 'Profi','Abrud', 'Sales'),
-
-        deputyPenny=new Experience(`${new Date('2013-01-10T14:00:00')}`,`${new Date('2014-11-10T14:00:00')}`,'Deputy Chief Shop','Penny Market','Sebes','Sales'),
-
-        workerKaufland=new Experience(`${new Date('2009-08-10T14:00:00')}`,`${new Date('2010-06-10T14:00:00')}`,'Commercial worker','Kaufland','Orastie','Sales'),
-
-        editorUniv=new Experience(`${new Date('2000-03-10T14:00:00')}`,`${new Date('2008-11-10T14:00:00')}`,'Copy Editor','Alba Iulia University','Alba Iulia','Printing / Publishing')
-
-    ],
-
-
-    calculateTime(date_start,date_end){
-
-
-        let distance = Math.abs(date_end - date_start);
-
-        const years=Math.floor(distance/(((3600000*24)*30)*12));
-        distance-=years*(((3600000*24)*30)*12);
-        const months=Math.floor(distance/((3600000*24)*30));
-        distance-=months*((3600000*24)*30);
-        const days = Math.floor(distance / (3600000*24));
-        distance -=days*(3600000*24);
-        const hours = Math.floor(distance / 3600000);
-        distance -= hours * 3600000;
-        const minutes = Math.floor(distance / 60000);
-        distance -= minutes * 60000;
-        const seconds = Math.floor(distance / 1000);
-        return ` ${years} years ${months} month ${days} days `;
-    
-
-
-
-    },
-
-
-    render(){
-        const renderHook=document.getElementById('experience');
-        const expList=document.createElement('ul');
-        for(exp of this.experience){
-            const expEl=document.createElement('li');
-            expEl.innerHTML=`
+  render() {
+    const renderHook = document.getElementById("experience");
+    const expList = document.createElement("ul");
+    for (exp of this.experience) {
+      const expEl = document.createElement("li");
+      expEl.innerHTML = `
             <div>
                 <h4>${exp.date_start} - ${exp.date_end}</h4>
                 <h3>${exp.position}</h3>
                 <p>Company: ${exp.company}</p>
                 <p>WorkTown: ${exp.workTown}</p>
                 <p>Fields of activities: ${exp.fieldOfActivities}</p>
-                <p>${(this.calculateTime(new Date(exp.date_start),new Date(exp.date_end)))}</p></ul>  
+                <p>${this.calculateTime(
+                  new Date(exp.date_start),
+                  new Date(exp.date_end)
+                )}</p>  
             </div>  
                     
             `;
-            expList.append(expEl);
-        }
-        renderHook.append(expList);
-    },
-
-
+      expList.append(expEl);
+    }
+    renderHook.append(expList);
+  },
 };
 
-window.addEventListener('load',myInit, true);
+window.addEventListener("load", myInit, true);
 
-function myInit(){
-    ageCalculation.addCurrentYear();
-    experienceList.render();
+function myInit() {
+  ageCalculation.addCurrentYear();
+  experienceList.render();
 }
--- script.js
+++ script.js
@@ -1,79 +1,59 @@
-let p_content=document.getElementById('header').innerHTML;
+let p_content = document.getElementById("header").innerHTML;
 
-let header={
-    'background-color':'yellow',
-    render:function(){
-        
-        const cevaCS=document.getElementById('menu-nav');
+let header = {
+  "background-color": "yellow",
+  render: function () {
+    const cevaCS = document.getElementById("menu-nav");
 
-        // console.log(p_content);
-        
-        // console.log(content['background-color']);
+    if (p_content == "Home") {
+      document.getElementById("header").innerHTML = "Marius Herlea HomePage.";
 
-        if(p_content=='Home'){
-            document.getElementById('header').innerHTML='Marius Herlea HomePage.';
+      cevaCS.style.backgroundColor = this["background-color"];
+      document.getElementById("header").style.backgroundColor = "aqua";
+    } else if (p_content == "CV") {
+      document.getElementById("header").innerHTML = "HM Curricullum Vitae";
 
-            // backgroundColor = background-color
-            
-            cevaCS.style.backgroundColor=this["background-color"];
-            document.getElementById('header').style.backgroundColor='aqua';
-
-        } else if(p_content=='CV'){
-
-            document.getElementById('header').innerHTML='Curricullum Vitae';
-
-            cevaCS.style.backgroundColor="cyan";
-            document.getElementById('header').style.backgroundColor='yellow';
-        
-        } else {
-        }
-    
-    },
-
-}
-
-let content={
-
-    print: function(){
-        var ul=document.getElementById('menu-nav');
-        var Home=document.createElement('button');
-        var CV=document.createElement('button');
-
-        Home.appendChild(document.createTextNode('Home'));
-        CV.appendChild(document.createTextNode('CV'));
-        
-        if(p_content=='Home'){
-            ul.appendChild(CV);
-        } else if(p_content=='CV'){
-            ul.appendChild(Home);
-        }
-
-        Home.onclick=function(){
-            window.location='index.html'
-        }
-        CV.onclick=function(){
-            window.location='cv.html'
-        }
+      cevaCS.style.backgroundColor = "cyan";
+      document.getElementById("header").style.backgroundColor = "yellow";
+    } else {
     }
-    
-}
+  },
+};
 
-let ageCalculation={
+let content = {
+  print: function () {
+    var ul = document.getElementById("menu-nav");
+    var Home = document.createElement("button");
+    var CV = document.createElement("button");
 
-    addCurrentYear:function(){
-        document.getElementById('curentYear').innerHTML = new Date().getFullYear();
+    Home.appendChild(document.createTextNode("Home"));
+    CV.appendChild(document.createTextNode("CV"));
+
+    if (p_content == "Home") {
+      ul.appendChild(CV);
+    } else if (p_content == "CV") {
+      ul.appendChild(Home);
     }
-    
-}
 
+    Home.onclick = function () {
+      window.location = "index.html";
+    };
+    CV.onclick = function () {
+      window.location = "cv.html";
+    };
+  },
+};
 
+let ageCalculation = {
+  addCurrentYear: function () {
+    document.getElementById("curentYear").innerHTML = new Date().getFullYear();
+  },
+};
 
+window.addEventListener("load", myInit, true);
 
-window.addEventListener('load',myInit, true);
-
-function myInit(){
-    header.render();
-    content.print();
-    ageCalculation.addCurrentYear();
-
+function myInit() {
+  header.render();
+  content.print();
+  ageCalculation.addCurrentYear();
 }
