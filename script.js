19ee062bc9078876a2707f5f206a503a8a4debc4
mariusherlea <mariusherlea@gmail.com>
2023-10-30 10:09:53 +0200

    let instead var

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ function weatherBalloon(cityID) {
       return resp.json();
     }) // Convert data to json
     .then(function (data) {
+      console.log(data);
       drawWeather(data);
     })
     .catch(function () {
@@ -82,18 +83,14 @@ window.onload = function () {
 };
 
 function drawWeather(d) {
-  var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
-  var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
-  var description = d.weather[0].description;
+  let celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+  let fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+  let description = d.weather[0].description;
+
+  document.getElementById("humidity").innerHTML =
+    d.main.humidity + " % humidity";
+  document.getElementById("presure").innerHTML = d.main.pressure + " hPa";
   document.getElementById("description").innerHTML = d.weather[0].description;
   document.getElementById("temp").innerHTML = celcius + "&deg;";
   document.getElementById("location").innerHTML = d.name;
-
-  if (description.indexOf("rain") > 0) {
-    document.body.className = "rainy";
-  } else if (description.indexOf("cloud") > 0) {
-    document.body.className = "cloudy";
-  } else if (description.indexOf("sunny") > 0) {
-    document.body.className = "sunny";
-  }
 }
