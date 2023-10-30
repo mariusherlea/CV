5eb2f4baf94c58e3d0cf3d2265c406bb6eaba6c6
mariusherlea <mariusherlea@gmail.com>
2023-10-30 09:37:30 +0200

    add open weather api

--- script.js
+++ script.js
@@ -57,3 +57,43 @@ function myInit() {
   content.print();
   ageCalculation.addCurrentYear();
 }
+
+function weatherBalloon(cityID) {
+  var key = "728d0c264572c8885e9253484a6de83b";
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?id=" +
+      cityID +
+      "&appid=" +
+      key
+  )
+    .then(function (resp) {
+      return resp.json();
+    }) // Convert data to json
+    .then(function (data) {
+      drawWeather(data);
+    })
+    .catch(function () {
+      // catch any errors
+    });
+}
+
+window.onload = function () {
+  weatherBalloon(679995);
+};
+
+function drawWeather(d) {
+  var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+  var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+  var description = d.weather[0].description;
+  document.getElementById("description").innerHTML = d.weather[0].description;
+  document.getElementById("temp").innerHTML = celcius + "&deg;";
+  document.getElementById("location").innerHTML = d.name;
+
+  if (description.indexOf("rain") > 0) {
+    document.body.className = "rainy";
+  } else if (description.indexOf("cloud") > 0) {
+    document.body.className = "cloudy";
+  } else if (description.indexOf("sunny") > 0) {
+    document.body.className = "sunny";
+  }
+}
